080236e68536f77e7259dbed8608a107ac3ccc36
Alena Trushnikova <alena.yu.t@gmail.com>
2020-10-12 17:58:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,12 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
+<<<<<<< HEAD
   base_hash = {:railroads => {}}
+=======
+  base_hash[:railroads] = []
+
+>>>>>>> c6689ab11d0a230f08e5a3a6f7a29f15d722a262
 end
 
 def monopoly_with_second_tier
